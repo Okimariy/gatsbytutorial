d38f48cb96bb945b2eb2eb6ebe0a48c350a91297
RPokimari <60766541+RPokimari@users.noreply.github.com>
2021-02-13 18:08:37 +0900

    Markdownファイルの内容取得

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,37 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-
-export default function Home() {
+export default function Home({ data }) {
   return(
     <div>
       <Layout>
           <h1>おきまりのサイト</h1>
+          {data.allMarkdownRemark.nodes.map(x => (
+            <div key={x.id}>
+              <h3>{x.frontmatter.title}</h3>
+              <p>{x.frontmatter.date}</p>
+              <div dangerouslySetInnerHTML={{ __html: x.html }} />
+            </div>
+          ))}
       </Layout>
     </div>
   ) 
 }
 
+
+export const query = graphql`
+  {
+    allMarkdownRemark {
+      nodes {
+        html
+        timeToRead
+        frontmatter {
+          date
+          title
+        }
+      }
+    }
+  }
+`
 
