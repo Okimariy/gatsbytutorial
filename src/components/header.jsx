2b355c55c0a9853a53469057e689e51e6330cb26
RPokimari <60766541+RPokimari@users.noreply.github.com>
2021-02-13 16:15:33 +0900

    useStaticQueryを使用して、pageコンポーネント以外でデータを取得

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,24 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, Link } from "gatsby"
+
+// ページコンポーネント以外のコンポーネントでは
+// useStaticQueryを利用してGrasphQLでデータを取得する
 
 const Header = () => {
+  const data = useStaticQuery(
+    graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+    `
+  )
   return(
     <header>
+      <h2>{data.site.siteMetadata.title}</h2>
       <nav>
         <ul>
           <li><Link to="/">トップ</Link></li>
